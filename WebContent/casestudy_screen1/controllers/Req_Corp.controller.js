d8f0f6a54fee5e7cbc60ac88ee70a64df64097e5
Pavan <pavankowshik@gmail.com>
2013-12-27 11:57:42 +0530

    some commit
    Change-Id: I75fe25e0488fcbdb92b67ce295060f4667906cf8
    Signed-off-by: Pavan <pavankowshik@gmail.com>

--- WebContent/casestudy_screen1/controllers/Req_Corp.controller.js
+++ WebContent/casestudy_screen1/controllers/Req_Corp.controller.js
@@ -1,5 +1,7 @@
 sap.ui.controller("casestudy_screen1.controllers.Req_Corp", {
 
+	//some comment 
+	
 /**
 * Called when a controller is instantiated and its View controls (if available) are already created.
 * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
