1c99fb0a3a5dad5d6c81f0ee76e5bd481ee39207
Kirti Vardhan Rathore <kirtivr.bit@gmail.com>
2014-01-03 14:51:39 +0530

    Integrating with Meet 1

--- WebContent/casestudy_screen1/controllers/Req_Corp.controller.js
+++ WebContent/casestudy_screen1/controllers/Req_Corp.controller.js
@@ -1,9 +1,5 @@
 sap.ui.controller("casestudy_screen1.controllers.Req_Corp", {
 
-	//some comment 
-	//xyz uvw
-	//one more comment
-	
 /**
 * Called when a controller is instantiated and its View controls (if available) are already created.
 * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
